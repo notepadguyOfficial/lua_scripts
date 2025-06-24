2199174c3432e9325e1cdeaeae81a53a0b3ffdfd
notepadguyOfficial <rerollhashtagnumber1@gmail.com>
2025-06-24 19:53:59 +0800

    Create quest_52.sql

---
+++ Easter Kingdoms/Elwynn Forest/quest_52.sql
@@ -0,0 +1,1 @@
+UPDATE `quest_template` SET `LogDescription` = 'Kill 8 Prowlers or Gray Forest Wolves and 5 Young Forest Bears, and then return to Guard Thomas at the east Elynn bridge.' WHERE (`ID` = 52);
